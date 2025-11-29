b2caa3d8d5da0214d422e9948a6c9d821f2462c9
2091a <anshulbhattglt@gmail.com>
2025-11-29 18:21:06 +0530

    sidebar and darkmode setup

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,20 +1,41 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
+"use client";
+import React, { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import StoreProvider, { useAppSelector } from './redux';
 
-type Props = {}
+const DasboardLayout = ({children}:{children: React.ReactNode}) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state)=>state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state)=>state.global.isDarkMode);
 
-const DasboardWrapper = ({children}:{children: React.ReactNode}) => {
+  useEffect(()=>{
+    if(isDarkMode){
+      document.documentElement.classList.add("dark")
+    }else{
+      document.documentElement.classList.remove("dark")
+    }
+  })
+
   return (
-    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900 ">
         {/*sidebar*/}
         <Sidebar/>
         
-        <main className='flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64'>
-            <Navbar></Navbar>
+        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${ isSidebarCollapsed ? "" :"md:pl-64"}`}>
+            <Navbar/>
             {children}
         </main>
     </div>
+  );
+};
+
+const DasboardWrapper = ({children}:{children: React.ReactNode}) => {
+  return(
+    <StoreProvider>
+      <DasboardLayout>{children}</DasboardLayout>
+    </StoreProvider>
   )
 }
 
